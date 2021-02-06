83406b3aadab955ea60216599d9afb1f6ae18f80
Shane Xannr <shane.xannr@outlook.com>
2021-02-06 17:27:52 -0500

    ADD:  started: constucted a screen of components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <app-inbox-screen></app-inbox-screen>
+  `,
 })
 export class AppComponent {
-  title = 'intro-storybook-angular-template';
+  title = 'taskbox';
 }
--- src/app/state/task.state.ts
+++ src/app/state/task.state.ts
@@ -1,3 +1,4 @@
+
 import { State, Selector, Action, StateContext } from '@ngxs/store';
 import { Task } from '../models/task.model';
 
@@ -5,6 +6,8 @@ import { Task } from '../models/task.model';
 export const actions = {
   ARCHIVE_TASK: 'ARCHIVE_TASK',
   PIN_TASK: 'PIN_TASK',
+  // defines the new error field we need
+  ERROR: 'APP_ERROR',
 };
 
 export class ArchiveTask {
@@ -18,6 +21,11 @@ export class PinTask {
 
   constructor(public payload: string) {}
 }
+// the class definition for our error field
+export class AppError {
+  static readonly type = actions.ERROR;
+  constructor(public payload: boolean) {}
+}
 
 // The initial state of our store when the app loads.
 // Usually you would fetch this from a server
@@ -30,6 +38,7 @@ const defaultTasks = {
 
 export class TaskStateModel {
   entities: { [id: number]: Task };
+  error: boolean;
 }
 
 // sets the default state
@@ -37,6 +46,7 @@ export class TaskStateModel {
   name: 'tasks',
   defaults: {
     entities: defaultTasks,
+    error: false,
   },
 })
 export class TasksState {
@@ -46,6 +56,13 @@ export class TasksState {
     return Object.keys(entities).map(id => entities[+id]);
   }
 
+  // defines a new selector for the error field
+  @Selector()
+  static getError(state: TaskStateModel) {
+    const { error } = state;
+    return error;
+  }
+  //
   // triggers the PinTask action, similar to redux
   @Action(PinTask)
   pinTask({ patchState, getState }: StateContext<TaskStateModel>, { payload }: PinTask) {
@@ -60,7 +77,7 @@ export class TasksState {
       entities,
     });
   }
-  // triggers the archiveTask action, similar to redux
+  // triggers the PinTask action, similar to redux
   @Action(ArchiveTask)
   archiveTask({ patchState, getState }: StateContext<TaskStateModel>, { payload }: ArchiveTask) {
     const state = getState().entities;
@@ -72,6 +89,15 @@ export class TasksState {
 
     patchState({
       entities,
+    });
+  }
+
+  // function to handle how the state should be updated when the action is triggered
+  @Action(AppError)
+  setAppError({ patchState, getState }: StateContext<TaskStateModel>, { payload }: AppError) {
+    const state = getState();
+    patchState({
+      error: !state.error,
     });
   }
 }
