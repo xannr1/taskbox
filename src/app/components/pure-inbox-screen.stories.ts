442a3d531d68245921670605bfb37841deb0b014
Shane Xannr <shane.xannr@outlook.com>
2021-02-06 17:42:39 -0500

    ADD: ended: constructed a screen of components.

---
+++ src/app/components/pure-inbox-screen.stories.ts
@@ -0,0 +1,27 @@
+
+import { moduleMetadata } from '@storybook/angular';
+import { PureInboxScreenComponent } from './pure-inbox-screen.component';
+import { TaskModule } from './task.module';
+import { Store, NgxsModule } from '@ngxs/store';
+import { TasksState } from '../state/task.state';
+export default {
+  title: 'PureInboxScreen',
+  decorators: [
+    moduleMetadata({
+      imports: [TaskModule, NgxsModule.forRoot([TasksState])],
+      providers: [Store],
+    }),
+  ],
+};
+// inbox screen default state
+export const Default = () => ({
+  component: PureInboxScreenComponent,
+});
+
+// inbox screen error state
+export const error = () => ({
+  component: PureInboxScreenComponent,
+  props: {
+    error: true,
+  },
+});
