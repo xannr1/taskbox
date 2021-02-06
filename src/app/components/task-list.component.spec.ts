38d253e03fde9ccd814ceafeb0d78b9660197fdd
Shane Xannr <shane.xannr@outlook.com>
2021-02-06 16:14:01 -0500

    ADD: task list component and storyshots

---
+++ src/app/components/task-list.component.spec.ts
@@ -0,0 +1,24 @@
+import { render } from '@testing-library/angular';
+import { TaskListComponent } from './task-list.component';
+import { TaskComponent } from './task.component';
+import { withPinnedTasksData } from './task-list.stories';
+describe('TaskList component', () => {
+  it('renders pinned tasks at the start of the list', async () => {
+    const mockedActions = jest.fn();
+    const tree = await render(TaskListComponent, {
+      declarations: [TaskComponent],
+      componentProperties: {
+        tasks: withPinnedTasksData,
+        loading: false,
+        onPinTask: {
+          emit: mockedActions,
+        } as any,
+        onArchiveTask: {
+          emit: mockedActions,
+        } as any,
+      },
+    });
+    const component = tree.fixture.componentInstance;
+    expect(component.tasksInOrder[0].id).toBe('6');
+  });
+});
